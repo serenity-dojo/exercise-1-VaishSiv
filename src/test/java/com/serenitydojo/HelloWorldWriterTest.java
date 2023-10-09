04f3d52247d0797cf362181f067a631c5ca00022
VaishSiv <sivvaish@gmail.com>
2023-10-09 20:15:12 +1000

    First Assignment Completed

---
+++ src/test/java/com/serenitydojo/HelloWorldWriterTest.java
@@ -0,0 +1,11 @@
+package com.serenitydojo;
+
+import org.junit.Test;
+
+public class HelloWorldWriterTest {
+    @Test
+    public void shouldWriteHelloWorldToTheConsole(){
+        HelloWorldWriter writer =new HelloWorldWriter();
+        writer.writeHelloWorld();
+    }
+}
